d129ee8e15fbc9324a0de2a5a987e5e12318e422
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-07 10:03:52 -0500

    add prettier and eslint

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
 const drc = require('docker-registry-client')
 
-const isImageNotFound = (err) =>
-    err && err.body && err.body.code === 'NotFoundError'
+const isImageNotFound = err =>
+  err && err.body && err.body.code === 'NotFoundError'
 
 /**
  * Checks if specific Docker image DOES NOT exist.
  * Resolves with true if the image DOES NOT exist, resolves with false for anything
  * else.
-*/
-const dockerImageNotFound = (repo) => {
+ */
+const dockerImageNotFound = repo => {
   const rar = drc.parseRepoAndRef(repo)
 
   const client = drc.createClientV2({
-    repo: rar
+    repo: rar,
   })
 
   const tagOrDigest = rar.tag || rar.digest
 
-  return new Promise((resolve) => {
-    client.getManifest({ ref: tagOrDigest }, (err) => {
+  return new Promise(resolve => {
+    client.getManifest({ ref: tagOrDigest }, err => {
       client.close()
 
       if (err) {
-        console.error('got an error fetching info about Docker image %s:%s', rar.canonicalName, tagOrDigest)
+        console.error(
+          'got an error fetching info about Docker image %s:%s',
+          rar.canonicalName,
+          tagOrDigest
+        )
 
         if (isImageNotFound(err)) {
           console.log('Got definite %s', err.body.code)
@@ -42,5 +46,5 @@ const dockerImageNotFound = (repo) => {
 }
 
 module.exports = {
-  dockerImageNotFound
+  dockerImageNotFound,
 }
